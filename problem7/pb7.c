c12a0af6e16b79ae24b9e1ee52cf4fe33fb916d1
OalidKhan <oalidkhan69@gmail.com>
2025-08-12 17:05:40 +0600

    added file 7

---
+++ problem7/pb7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    double P,T,R,SI;
+    printf("Enter Principle: ");
+    scanf("%lf",&P);
+    printf("Enter Time: ");
+    scanf("%lf",&T);
+    printf("Enter Rate: ");
+    scanf("%lf",&R);
+    SI = (P*T*R)/100;
+    printf("Your Simple Interest: %.2lf",SI);
+    return 0;
+}
