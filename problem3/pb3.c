5b29c50cae2bd2202c6fb3c0604c1e6b646f6f42
OalidKhan <oalidkhan69@gmail.com>
2025-08-12 16:24:16 +0600

    added file 3

---
+++ problem3/pb3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    printf("Enter your value in days: ");
+    scanf("%d",&N);
+    printf("%d years\n",N/365);
+    printf("%d weeks\n",(N%365)/7);
+    printf("%d days",(N%365)%7);
+
+    return 0;
+}
