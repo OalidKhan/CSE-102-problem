9df67959d15598518c8d04f9db64e55acd7642ba
OalidKhan <oalidkhan69@gmail.com>
2025-08-12 16:21:20 +0600

    added file 2

---
+++ problem2/pb2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter your a value: ");
+    scanf("%d",&a);
+    printf("Enter your b value: ");
+    scanf("%d",&b);
+    if (a==b)
+    {
+        printf("The numbers are equal");
+    }
+    else
+    {
+        printf("The numbers are not equal");
+    }
+    return 0;
+}
