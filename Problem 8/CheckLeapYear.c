5f685746a77fafec885eec0959e4ec6218449650
Oalid Khan <oalidkhan69@gmail.com>
2025-10-09 23:18:53 +0600

    Update and rename pb8.c to CheckLeapYear.c

--- Problem 8/CheckLeapYear.c
+++ Problem 8/CheckLeapYear.c
@@ -4,9 +4,13 @@ int main()
     int YEAR;
     printf("Enter your year: ");
     scanf("%d",&YEAR);
-    if (YEAR%400==0 || YEAR%100==0 || YEAR%4==0)
+    if ((YEAR%400==0) || (YEAR%100!=0 && YEAR%4==0))
+    {
         printf("%d is a Leap Year",YEAR);
+    }
     else
+    {
         printf("%d is not a Leap Year",YEAR);
+    }
     return 0;
 }
