4ba9b544e9870cb0dad54cda1d2a0a28fa1852a0
Oalid Khan <oalidkhan69@gmail.com>
2025-10-22 16:59:21 +0600

    Update FindSecondMax.c

--- Find Second Maximum From Array/FindSecondMax.c
+++ Find Second Maximum From Array/FindSecondMax.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int N;
+    int N,max1,max2;
+    printf("Enter the size of Array: ");
     scanf("%d",&N);
     int arr[N];
+    printf("Enter the elements of Array: ");
     for(int i=0 ; i<N ; i++)
     {
         scanf("%d",&arr[i]);
     }
-    int max1,max2;
     if(arr[0]>arr[1])
     {
         max1 = arr[0];
@@ -26,8 +27,11 @@ int main()
             max2 = max1;
             max1 = arr[i];
         }
+        else if(arr[i]<max1 && arr[i]>max2)
+        {
+            max2 = arr[i];
+        }
     }
-    printf("%d\n%d",max1,max2);
-
+    printf("Second largest number: %d",max2);
     return 0;
 }
