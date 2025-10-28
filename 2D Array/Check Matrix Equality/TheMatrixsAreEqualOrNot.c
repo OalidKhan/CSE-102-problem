8c606a23abd7ee38de628fd0efe4a88ad7ae6701
Oalid Khan <oalidkhan69@gmail.com>
2025-10-29 00:22:05 +0600

    Create TheMatrixsAreEqualOrNot.c

---
+++ 2D Array/Check Matrix Equality/TheMatrixsAreEqualOrNot.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+    int r1,c1,r2,c2,count=0;
+    printf("Enter Row and Column for the first matrix: ");
+    scanf("%d %d",&r1,&c1);
+    int arr[r1][c1];
+    printf("Enter elements: \n");
+    for(int i=0 ; i<r1 ; i++)
+    {
+        for(int j=0 ; j<c1 ; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("Enter Row and Column for the second matrix: ");
+    scanf("%d %d",&r2,&c2);
+    int Arr[r2][c2];
+    printf("Enter elements: \n");
+    for(int i=0 ; i<r2 ; i++)
+    {
+        for(int j=0 ; j<c2 ; j++)
+        {
+            scanf("%d",&Arr[i][j]);
+        }
+    }
+    if(r1!=r2 || c1!=c2)
+    {
+        printf("The Row and Column are not equal !!!!");
+    }
+    else
+    {
+        int r=r1,c=c1;
+        for(int i=0 ; i<r ; i++)
+        {
+            for(int j=0 ; j<c ; j++)
+            {
+                if(arr[i][j]!=Arr[i][j])
+                {
+                    count++;
+                }
+            }
+        }
+        if(count==0)
+        {
+            printf("The matrixs are equal.");
+        }
+        else
+        {
+            printf("The matrixs are not equal.");
+        }
+    }
+
+    return 0;
+}
